ee61def9aad1beb388020cb968265a26ceb3e7da
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2024-02-03 19:20:05 +0600

    Add files via upload

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -26,3 +26,18 @@ mainMenuBlock.addEventListener('click', function (event) {
 hamburgerClose.addEventListener('click', function () {
     mainMenuBlock.classList.remove("show-menu");
 });
+
+
+function openLink(evt, animName) {
+    var i, x, tabLinks;
+    x = document.getElementsByClassName("mapContent");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabBtnLink");
+    for (i = 0; i < x.length; i++) {
+        tabLinks[i].classList.remove("tab-active");
+    }
+    document.getElementById(animName).style.display = "flex";
+    evt.currentTarget.classList.add("tab-active");
+}
